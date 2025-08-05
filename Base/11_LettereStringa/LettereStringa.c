f660d2ef9f11acd40d232e3e39f1cde3c7161563
Matteo <134586540+Mattepl22@users.noreply.github.com>
2025-08-05 16:50:07 +0200

    Starts with strings/characters exercises

---
+++ Base/11_LettereStringa/LettereStringa.c
@@ -0,0 +1,12 @@
+//Programma che prende una stringa data dall'utente e conta quante volte è ripetuta una certa lettera
+
+#include <stdio.h>
+
+int main(){
+    char string[500] = {}, carattere = ' ';
+
+    printf("Inserisci la stringa interessata:\n");
+    fgets(string, sizeof(string), stdin);
+
+    
+}
